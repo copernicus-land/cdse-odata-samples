0d0c58032944489dc4965088c9ef5be5b3419f26
Stijn Caerts <stijn.caerts@vito.be>
2025-12-01 11:05:48 +0100

    VLCC: update samples to new metadata specification

--- scripts/generate_local.sh
+++ scripts/generate_local.sh
@@ -5,10 +5,12 @@ dir=$(dirname $0)
 DOCKER_IMAGE="vito-docker.artifactory.vgt.vito.be/clms_metadata:latest"
 docker image pull $DOCKER_IMAGE
 
-while IFS="," read -r name path
+while IFS="," read -r enabled name path
 do 
     echo $name
-    # use VSICACHED option to circumvent libhdf5 writing to stdout
-    # using virtual filesystem with netCDF driver in Docker requires userfaultd system call, so set security-opt to allow it
-    docker run --rm -e VSICACHED=true --security-opt seccomp=unconfined -a stdout -a stderr -v /data/MTDA/:/data/MTDA/ -v /vitodata/:/vitodata/ $DOCKER_IMAGE odata create $path | python3 -m json.tool --indent 2 > $name.json
+    if [[ $enabled == true || $enabled == 1 ]]; then
+        # use VSICACHED option to circumvent libhdf5 writing to stdout
+        # using virtual filesystem with netCDF driver in Docker requires userfaultd system call, so set security-opt to allow it
+        docker run --rm -e VSICACHED=true --security-opt seccomp=unconfined -a stdout -a stderr -v /data/:/data/ -v /vitodata/:/vitodata/ $DOCKER_IMAGE odata create $path | python3 -m json.tool --indent 2 > $name.json
+    fi
 done < <(tail -n +2 $dir/local.csv)
