6dc1ee6a9dec959929be2df58f34f6c78e144588
Stijn Caerts <stijn.caerts@vito.be>
2026-07-08 18:57:13 +0200

    feat: combine ST PPI and QFLAG into a single OData product

--- scripts/generate_s3.sh
+++ scripts/generate_s3.sh
@@ -8,7 +8,7 @@ docker image pull $DOCKER_IMAGE
 
 profiles=$(aws configure list-profiles)
 
-while IFS="," read -r enabled profile name path
+while IFS="," read -r enabled profile name path custom_link
 do 
     echo $name
     if [[ $enabled == true || $enabled == 1 ]]; then
@@ -16,7 +16,12 @@ do
         echo "Using AWS CLI profile $profile_name"
         eval $(aws configure export-credentials --format env --profile $profile_name)
         export AWS_ENDPOINT_URL=$(aws configure --profile $profile_name get endpoint_url)
+        if [[ -n "$custom_link" ]]; then
+            export LINK__VALUE=$custom_link
+        fi
         # using virtual filesystem with netCDF driver in Docker requires userfaultd system call, so set security-opt to allow it
-        docker run --rm --security-opt seccomp=unconfined -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e AWS_ENDPOINT_URL -a stdout -a stderr  $DOCKER_IMAGE odata create $path | python3 -m json.tool --indent 2 > $name.json
+        docker run --rm --security-opt seccomp=unconfined -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e AWS_ENDPOINT_URL -e LINK__VALUE -a stdout -a stderr  $DOCKER_IMAGE odata create $path | python3 -m json.tool --indent 2 > $name.json
+	    # ~/PycharmProjects/clms-metadata/.venv/bin/python -m clms_metadata odata create $path | python3 -m json.tool --indent 2 > $name.json
+        unset LINK__VALUE
     fi
 done < <(tail -n +2 $dir/s3.csv)
